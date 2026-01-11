6b0faeda09dae767227ab8f412264690b4c7ab2b
PolinaDerevetska <pauline.deretska@krfk.kai.edu.ua>
2026-01-11 22:44:36 +0200

    feat(talk): add personality selection and conversation flow

--- src/bot.py
+++ src/bot.py
@@ -1,14 +1,17 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 
-from src.config import BOT_TOKEN
-from handlers import start, random, random_button, gpt, message_handler
+from config import BOT_TOKEN
+from handlers import start, random, random_button, gpt, message_handler, talk, talk_button
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("random", random))
 app.add_handler(CommandHandler("gpt", gpt))
+app.add_handler(CommandHandler("talk", talk))
 app.add_handler(CallbackQueryHandler(random_button, pattern='^(random|start)$'))
+app.add_handler(
+    CallbackQueryHandler(talk_button, pattern='^(talk_linus_torvalds|talk_guido_van_rossum|talk_mark_zuckerberg)$'))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
 
 app.run_polling(drop_pending_updates=True, allowed_updates=Update.ALL_TYPES)
--- src/handlers.py
+++ src/handlers.py
@@ -1,9 +1,10 @@
 import logging
+from random import choice
 
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from src.config import CHATGPT_TOKEN
+from config import CHATGPT_TOKEN
 from gpt import ChatGPTService
 from utils import (send_image, send_text, load_message, show_main_menu, load_prompt, send_text_buttons)
 
@@ -89,3 +90,124 @@ async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 chat_id=update.effective_chat.id,
                 message_id=waiting_message.message_id
             )
+    if conversation_state == "talk":
+        personality = context.user_data.get("selected_personality")
+        if personality:
+            prompt = load_prompt(personality)
+            chatgpt_service.set_prompt(prompt)
+        else:
+            await send_text(update, context, "Спочатку оберіть особистість для розмови!")
+            return
+        waiting_message = await send_text(update, context, "...")
+        try:
+            response = await chatgpt_service.add_message(message_text)
+            buttons = {"start": "Закінчити"}
+            personality_name = personality.replace("talk_", "").replace("_", " ").title()
+            await send_text_buttons(update, context, f"{personality_name}: {response}", buttons)
+        except Exception as e:
+            logger.error(f"Помилка при отриманні відповіді від ChatGPT: {e}")
+            await send_text(update, context, "Виникла помилка при отриманні відповіді!")
+            await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=waiting_message.message_id)
+        finally:
+            await context.bot.delete_message(
+                chat_id=update.effective_chat.id,
+                message_id=waiting_message.message_id
+            )
+    if not conversation_state:
+        intent_recognized = await inter_random_input(update, context, message_text)
+        if not intent_recognized:
+            await show_funny_response(update, context)
+        return
+
+
+async def talk(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data.clear()
+    await send_image(update, context, "talk")
+    personalities = {
+        'talk_linus_torvalds': "Linus Torvalds (Linux, Git)",
+        'talk_guido_van_rossum': "Guido van Rossum (Python)",
+        'talk_mark_zuckerberg': "Mark Zuckerberg (Meta, Facebook)",
+        'start': "Закінчити",
+    }
+    await send_text_buttons(update, context, "Оберіть особистість для спілкування ...", personalities)
+
+
+async def talk_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    data = query.data
+    if data == "start":
+        context.user_data.pop("conversation_state", None)
+        context.user_data.pop("selected_personality", None)
+        await start(update, context)
+        return
+    if data.startswith("talk_"):
+        context.user_data.clear()
+        context.user_data["selected_personality"] = data
+        context.user_data["conversation_state"] = "talk"
+        prompt = load_prompt(data)
+        chatgpt_service.set_prompt(prompt)
+        personality_name = data.replace("talk_", "").replace("_", " ").title()
+        await send_image(update, context, data)
+        buttons = {'start': "Закінчити"}
+        await send_text_buttons(
+            update,
+            context,
+            f"Hello, I`m {personality_name}."
+            f"\nI heard you wanted to ask me something. "
+            f"\nYou can ask questions in your native language.",
+            buttons
+        )
+
+
+async def inter_random_input(update: Update, context: ContextTypes.DEFAULT_TYPE, message_text):
+    message_text_lower = message_text.lower()
+    if any(keyword in message_text_lower for keyword in ['факт', 'цікав', 'random', 'випадков']):
+        await send_text(
+            update,
+            context,
+            text="Схоже, ви цікавитесь випадковими фактами! Зараз покажу вам один..."
+        )
+        await random(update, context)
+        return True
+
+    elif any(keyword in message_text_lower for keyword in ['gpt', 'чат', 'питання', 'запита', 'дізнатися']):
+        await send_text(
+            update,
+            context,
+            text="Схоже, у вас є питання! Переходимо до режиму спілкування з ChatGPT..."
+        )
+        await gpt(update, context)
+        return True
+
+    elif any(keyword in message_text_lower for keyword in ['розмов', 'говори', 'спілкува', 'особист', 'talk']):
+        await send_text(
+            update,
+            context,
+            text="Схоже, ви хочете поговорити з відомою особистістю! Зараз покажу вам доступні варіанти..."
+        )
+        await talk(update, context)
+        return True
+    return False
+
+
+async def show_funny_response(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    funny_responses = [
+        "Хмм... Цікаво, але я не зрозумів, що саме ви хочете. Може спробуєте одну з команд з меню?",
+        "Дуже цікаве повідомлення! Але мені потрібні чіткіші інструкції. Ось доступні команди:",
+        "Ой, здається, ви мене застали зненацька! Я вмію багато чого, але мені потрібна конкретна команда:",
+        "Вибачте, мої алгоритми не розпізнали це як команду. Ось що я точно вмію:",
+        "Це повідомлення таке ж загадкове, як єдиноріг у дикій природі! Спробуйте одну з цих команд:",
+        "Я намагаюся зрозуміти ваше повідомлення... Але краще скористайтесь однією з команд:",
+        "О! Випадкове повідомлення! Я теж вмію бути випадковим, але краще використовуйте команди:",
+        "Гм, не спрацювало. Може спробуємо ці команди?",
+        "Це повідомлення прекрасне, як веселка! Але для повноцінного спілкування спробуйте:",
+        "Згідно з моїми розрахунками, це повідомлення не відповідає жодній з моїх команд. Ось вони:",
+    ]
+    random_response = choice(funny_responses)
+    available_commands = """
+    - Не знаєте, що обрати? Почніть з /start,
+    - Спробуйте команду /gpt, щоб задати питання,
+    """
+    full_message = f"{random_response}\n{available_commands}"
+    await update.message.reply_text(full_message)
